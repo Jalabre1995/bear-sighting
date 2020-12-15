3f72d7dea21aca77628e74204f835d55a1fa433a
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-12-15 12:18:31 -0500

    created an enviorment variable for the api and created an InfroWindow that will get the sighting when the bear was spotted

--- bear-sighting/src/App.js
+++ bear-sighting/src/App.js
@@ -2,18 +2,18 @@
 import './App.css';
 import React from 'react';
 import {
-  GoogleMap, 
+  GoogleMap,
   useLoadScript,
   Marker,
-  InfroWindow,
+  InfoWindow,
 } from '@react-google-maps/api';
-import { formRelative } from 'date-fns';
+import { formatRelative } from 'date-fns';
 
 import '@reach/combobox/styles.css';
 import mapStyles from './mapStyles';
 
 const libraries = ['places'];
-const mapContainerStyle= {
+const mapContainerStyle = {
   widht: "100w",
   height: "100vh",
 };
@@ -24,7 +24,7 @@ const center = {
 }
 
 const options = {
-  styles:mapStyles,
+  styles: mapStyles,
   disableDefaultUI: true,
   zoomControl: true,
 
@@ -32,15 +32,16 @@ const options = {
 }
 
 export default function App() {
-  const {isLoaded, loadError} = useLoadScript({
+  const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     libraries,
   });
-  const [ markers, setMarkers] = React.useState([]);
+  const [markers, setMarkers] = React.useState([]);
+  const [selected, setSelected] = React.useState(null);
 
   ///Create  a new hook call callback //////
   //// This allows you to call a function that has the same value unless the depths change  
-  const onMapClick = React.useCallback((event) =>{
+  const onMapClick = React.useCallback((event) => {
     setMarkers((current) => [
       ...current,
       {
@@ -51,7 +52,7 @@ export default function App() {
     ])
   }, [])
 
-  //Add another variable
+  //Add another varibale
 
   const mapRef = React.useRef();
   const onMapLoad = React.useCallback((map) => {
@@ -60,28 +61,56 @@ export default function App() {
 
   ///Create some if statements if there was an load error////
   if (loadError) return 'Error loading Maps';
-  if(!isLoaded) return 'Loading Maps';
+  if (!isLoaded) return 'Loading Maps';
 
   return (
     <div>
       <h1>Bears</h1>
-  <GoogleMap
-  mapContainerStyle= {mapContainerStyle}
-  zoom={8}
-  center={center}
-  options={options}
-    onClick={onMapClick}
-    onLoad= {onMapLoad}
-  >
-    {markers.map(marker => <Marker key={marker.time.toISOString()}
-    position= {{lat: marker.lat, lng: marker.lng}} />)}
-    icon= {{
-      url:"./bear.svg",
-      scaledSize: new window.google.maps.Size(30,30),
-      origin: new window.google.maps.Points(0,0),
-      anchor: new window.google.mpas.Points(15, 15),
-    }}
-  </GoogleMap>
-  </div>
-  )
-};
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={8}
+        center={center}
+        options={options}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+      >
+        {markers.map((marker =>
+          <Marker
+            key={marker.time.toISOString()}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+            icon={{
+              url: "./bear.svg",
+              scaledSize: new window.google.maps.Size(30, 30),
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(15, 15),
+            }}
+          />
+        ))}
+
+        {selected ? (
+          <InfoWindow
+          ///This is going to pop up the white window and its goign to append a child, showing when the bear was spotted.
+          ///I am also going to use the form relative where its going to take two dates and spit up a value that is related to now..
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <h2>
+                <span role='img' aria-label='bear'>
+                  🐻
+                </span>{''}
+                    Alert
+                  </h2>
+              <p>Spotted {formatRelative(selected.time, new Date())}</p>
+            </div>
+          </InfoWindow>
+        ) : null}
+      </GoogleMap>
+    </div>
+  );
+}
